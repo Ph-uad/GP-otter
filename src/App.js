c1904efccea2946a9bfa9398ba2859019589f2cd
Ph-uad <temidayo26j@gmail.com>
2023-05-15 11:26:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Navbar,  Brand, CTA } from './components'
+import { Blog,Header, Feature, Footer, Possibility, WhatGp3 } from './container'
 
 const App = () => {
     return (
-        <div>Let's Go</div>
+        <div className='App'>
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGp3 />
+            <Feature />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+
+        </div>
     )
 }
 
---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from './blog/Blog.js';
+export { default as Feature } from './feature/Feature.js';
+export { default as Footer } from './footer/Footer.js';
+export { default as Header } from './header/Header.js';
+export { default as Possibility } from './possibility/Possibility.js';
+export { default as WhatGp3 } from './whatGPT3/WhatGPT3.js';
