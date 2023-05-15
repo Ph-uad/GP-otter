2d3f6319db59987dcdbe7bb990817872655a53e1
Ph-uad <temidayo26j@gmail.com>
2023-05-15 10:12:26 +0100

    init

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from './article/Article.js';
+export { default as Brand } from './brand/Brand.js';
+export { default as CTA } from './cta/CTA.js';
+export { default as Feature } from './feature/Feature.js';
+export { default as Navbar } from './navbar/Navbar.js';
---
+++ src/container/possibility/Posibility.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Posibility.module.css'
+
+const Posibility = () => {
+  return (
+    <div>Posibility</div>
+  )
+}
+
+export default Posibility
